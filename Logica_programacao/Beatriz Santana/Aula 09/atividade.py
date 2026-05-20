9c5a27df7b14cf4c337b90fd0f947f850d6709d1
Beatriz Santana <0307091bia@gmail.com>
2026-05-20 08:53:57 -0300

    atividades

--- Logica_programacao/Beatriz Santana/Aula 09/atividade.py
+++ Logica_programacao/Beatriz Santana/Aula 09/atividade.py
@@ -57,17 +57,104 @@
 # else:
 #     print("Senha incorreta tente novamente")
 
-# 5. Atribuição vs. Comparação
-# clima = "ensolarado"
-# if clima = "chuvoso":
-# print("Leve um guarda-chuva!")
+# # 5. Atribuição vs. Comparação
+# # clima = "ensolarado"
+# # if clima = "chuvoso":
+# # print("Leve um guarda-chuva!")
 # Corrigido
 # clima = "chuvoso"
 # if clima == "chuvoso":
 #     print(f"Leve um guarda-chuva!")
+# # Melhorado
+# clima = input("Como está o dia hoje?: ")
+# if clima == "chuvoso":
+#     print(f"Leve um guarda chuva e tome cuidado!")
+# else:
+#     print(f"Aproveite o dia!")
+
+
+# 6. Misturando Alhos com Bugalhos
+# pontos = 50
+# print("Parabéns! Você fez " + pontos + " pontos.")
+# Corrigido
+# pontos = 50
+# print(f"Parabéns! Você fez {pontos} pontos.")
 # Melhorado
-clima = input("Como está o dia hoje?: ")
-if clima == "chuvoso":
-    print(f"Leve um guarda chuva e tome cuidado!")
+# pontos = input("Quantos pontos você fez?: ")
+# print(f"Parabéns! Você fez {pontos} pontos.")
+
+# 7. A Ordem dos Fatores
+# O sistema deve dar "Excelente" para notas 9 ou 10.
+# nota = 9.5
+# if nota >= 7:
+# print("Aprovado")
+# elif nota >= 9:
+# print("Excelente!")
+# Corrigido
+# nota = "9.5"
+# if nota >= "9":
+#     print(f"Excelente!")
+# elif nota >= "7":
+#     print(f"Aprovado")
+# Melhorado
+# nota = input("Qual foi a nota tirada?: ")
+# if nota >= "9":
+#     print(f"Sua nota foi excelente!")
+# elif nota >= "6":
+#     print(f"Você foi aprovado")
+# else:
+#     print(f"Você pode melhorar!")   
+
+
+# 8. O Contador de 1 a 5
+# Objetivo: Mostrar na tela os números 1, 2, 3, 4 e 5.
+# for i in range(5):
+# print(i)
+# Corrigido
+# for i in range(6):
+#  print(i)
+# Melhorado
+# print("Será iniciada a contagem regressiva")
+# for i in range(6):
+#  print(i)
+
+
+# 9. O Loop Eterno
+# tentativas = 1
+# while tentativas <= 3:
+# print("Tentando conectar...")
+# O código deveria parar após 3 tentativas
+# Corrigido
+# tentativas = 1
+# while tentativas <= 3:
+#     tent = int(input("Digite a quantidade"))
+#     for tentativas in range(tent):       
+#         print("Tentando conectar...") 
+    # break
+#  O código deveria parar após 3 tentativas
+# Melhorado
+# tentativas = 1
+# while tentativas <= 3:
+#  print("Tentando conectar...")
+#  tentativas += 1
+# print("Tentativas esgotadas. Conexão falhou.")
+
+
+# 10. A Senha Teimosa
+# O programa deve pedir a senha até que o usuário digite "python123"
+# senha = ""
+# while senha == "python123":
+# senha = input("Digite a senha secreta: ")
+# print("Acesso concedido!")
+# Corrigido
+# senha = input(f"Digite a senha secreta: ")
+# while senha == "python123":
+#    print("Acesso concedido!")
+#    break
+# # Melhorado
+senha = input(f"Olá! digite a senha secreta: ")
+while senha == "python123":
+   print("Acesso concedido!")
+   break
 else:
-    print(f"Aproveite o dia!")
+   print("Senha incorreta tente novamente!")
